2727b35b3e7a78fcad3af54d5065000c0a8c1262
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-15 22:26:54 -0300

    Configurando Vue Routes, Vuex, e Vue Resource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
+import { sync } from 'vuex-router-sync'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/index'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+sync(store, router);
+
 
 new Vue({
   router,
   store,
+  Vuex,
+  VueResource,
   render: h => h(App)
 }).$mount('#app')
